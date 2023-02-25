64327f82213a24bdce6cfce0ee123753a6a42947
Oliver Benz <oliver.benz@outlook.com>
2023-02-25 10:47:02 +0100

    Add logfile in example.

--- examples/standard/main.cpp
+++ examples/standard/main.cpp
@@ -3,15 +3,18 @@
 #include "LogLevel.hpp"
 #include "LogOutputConsole.hpp"
 #include "LogOutputMock.hpp"
+#include "LogOutputFile.hpp"
 
 int main(){
 	std::cout << "Version: " << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << std::endl;
 	
 	static Logging::LogConfig config;
-	config.AddLogOutput(std::make_shared<Logging::LogOutputConsole>());
-
 	auto mock = std::make_shared<Logging::LogOutputMock>();
+	auto logFile = std::make_shared<Logging::LogOutputFile>("Filename.txt");
+
+	config.AddLogOutput(std::make_shared<Logging::LogOutputConsole>());
 	config.AddLogOutput(mock);
+	config.AddLogOutput(logFile);
 
 	{
 		Logging::Logger logger = Logging::Logger(config);
@@ -24,6 +27,9 @@ int main(){
 	for (const auto& entry : mock->m_logEntries){
 		std::cout << LevelToText(entry.GetLevel()) + " " + entry.GetText() + "\n";
 	}
+
+	// Cleanup log file
+	std::remove(logFile->FilePath().c_str());
 
 	return 0;
 }
