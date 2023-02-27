f839aab5da9570e7913eca62ef503f8f55f549f9
Oliver Benz <oliver.benz@outlook.com>
2023-02-27 16:13:46 +0100

    GTest: Prepare unit tests.

---
+++ tests/LogEntry.gtest.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+#include "LogEntry.hpp"
+
+namespace Logging {
+namespace GTest {
+
+TEST(LogEntry, OutputFormat) {
+    // TODO:
+    //   - Check the output string has proper format. Should formatting be job of entry? For now ok.
+    //   - Use all functions once so it's covered..
+
+}
+
+}
+}
