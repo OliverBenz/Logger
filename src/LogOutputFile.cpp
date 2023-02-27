ab141372e5c113e30397e109b826c1f39546ddb4
Oliver Benz <oliver.benz@outlook.com>
2023-02-27 15:48:15 +0100

    LogOutputFile: Add maximum filesize option.

--- src/LogOutputFile.cpp
+++ src/LogOutputFile.cpp
@@ -1,11 +1,33 @@
 #include "LogOutputFile.hpp"
 
 #include <fstream>
+#include <filesystem>
 
 namespace Logging {
 
-LogOutputFile::LogOutputFile(const std::string& filePath) : m_filePath(filePath)
+LogOutputFile::LogOutputFile(const std::string& filePath, std::size_t maxFileSize)
+    : m_filePath(filePath), m_maxFileSize(maxFileSize)
 {}
+
+void LogOutputFile::RotateFile() {
+    // TODO: Test this function
+    std::size_t pos = m_filePath.rfind('.');
+    const std::string path = m_filePath.substr(0, pos);
+    const std::string ext = m_filePath.substr(pos+1);
+
+    // New filename of logFile has postfix [number].
+    unsigned count = 0;
+    std::string newFilePath;
+    do {
+        newFilePath = path + "[" + std::to_string(count) + "]." + ext;
+        ++count;
+    } while(std::filesystem::exists(newFilePath));
+
+    // The current file is renamed with the postfix.
+    if (!std::rename(m_filePath.c_str(), newFilePath.c_str()) == 0) {
+        // throw std::ios_base::failure(std::stderror(-1));
+    }
+}
 
 void LogOutputFile::Write(const std::vector<LogEntry>& logEntries) {
     std::unique_lock<std::mutex> lock(m_writeLock);
@@ -19,6 +41,13 @@ void LogOutputFile::Write(const std::vector<LogEntry>& logEntries) {
     for (const auto& entry : logEntries) {
         outfile << entry.OutputText() << "\n";
     }
+
+    // Check max filesize reached
+    const auto fileSize = outfile.tellp();
+    outfile.close();
+    if (fileSize != std::ofstream::pos_type(-1) && static_cast<std::uintmax_t>(fileSize) >= m_maxFileSize) {
+        RotateFile();
+    }
 }
 
 void LogOutputFile::Write(const LogEntry& entry) {
@@ -31,6 +60,13 @@ void LogOutputFile::Write(const LogEntry& entry) {
     }
 
     outfile << entry.OutputText() << "\n";
+
+    // Check max filesize reached
+    const auto fileSize = outfile.tellp();
+    outfile.close();
+    if (fileSize != std::ofstream::pos_type(-1) && static_cast<std::uintmax_t>(fileSize) >= m_maxFileSize) {
+        RotateFile();
+    }
 }
 
 std::string LogOutputFile::FilePath() const {
--- src/include/LogOutputFile.hpp
+++ src/include/LogOutputFile.hpp
@@ -8,17 +8,30 @@ namespace Logging {
 
 class LogOutputFile : public ILogOutput {
 public:
-    LogOutputFile(const std::string& filePath);
+    //! The current log file is always the one mentioned in 'filePath'.
+    /*!
+     * \param[in] filePath Path to the log file.
+     * \param[in] maxFileSize Maximum file size in bytes before starting a new file (4MB default).
+     */
+    LogOutputFile(const std::string& filePath, std::uintmax_t maxFileSize = 4 * 1024 * 1024);
 
+    //! Write set of log entries to the logfile.
 	void Write(const std::vector<LogEntry>& logEntries) override;
+    
+    //! Write single log entry to the logfile.
     void Write(const LogEntry& entry) override;
 
     //! Get the filepath as entered in the constructor.
     std::string FilePath() const;
 
 private:
-    std::string m_filePath;  //!< Path to the log file.
-    std::mutex m_writeLock;  //!< Lock so file is only opened on one thread.
+    //! Rename the current log file and create a new one.
+    void RotateFile();
+
+private:
+    std::string m_filePath;       //!< Path to the log file.
+    std::uintmax_t m_maxFileSize; //!< Maximum size of a logfile before starting a new file.
+    std::mutex m_writeLock;       //!< Lock so file is only opened on one thread.
 };
 
 }
