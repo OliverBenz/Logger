9511a6d2ec5c22a696e5b56d64e5818626624758
Oliver Benz <oliver.benz@outlook.com>
2023-04-07 18:28:40 +0200

    Logger: Add profiler class with fmt library.

---
+++ src/Profiler.cpp
@@ -0,0 +1,28 @@
+#include "Profiler.hpp"
+#include <fmt/core.h>
+
+namespace Logging {
+
+Profiler::Profiler(Logger logger, std::string identifier)
+	: m_logger(std::move(logger)), m_identifier(std::move(identifier))
+{
+	m_startTime = std::chrono::steady_clock::now();
+	m_lastTime = m_startTime;
+
+	m_logger.Log(Logging::LogLevel::Info, fmt::format("--> {} START", m_identifier));
+}
+
+Profiler::~Profiler() {
+	const auto now = std::chrono::steady_clock::now();
+	const auto timeMs = std::chrono::duration_cast<std::chrono::milliseconds>(now - m_startTime).count();  // t in ms
+	m_logger.Log(Logging::LogLevel::Info, fmt::format("<-- {} END: ", m_identifier, timeMs));
+}
+
+void Profiler::LogStep(const std::string& stepName) {
+	const auto timeMs = std::chrono::duration_cast<std::chrono::milliseconds>(m_lastTime - m_startTime).count();  // t in ms
+	m_logger.Log(Logging::LogLevel::Info, fmt::format("--- {} STEP {}: ", m_identifier, stepName, timeMs));
+	m_lastTime = std::chrono::steady_clock::now();
+}
+
+
+}
