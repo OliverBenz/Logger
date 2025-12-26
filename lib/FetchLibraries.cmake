23b633337ce5ef5191dcd3adb3830de2b451152d
Oliver Benz <oliver.benz@outlook.com>
2025-12-26 14:32:54 +0100

    CMake: Update gtest version.

--- lib/FetchLibraries.cmake
+++ lib/FetchLibraries.cmake
@@ -3,11 +3,10 @@ cmake_minimum_required(VERSION 3.16)
 include(FetchContent)
 
 # GTest
-# Version 1.13.0
 FetchContent_Declare(
 		googletest
 		GIT_REPOSITORY https://github.com/google/googletest.git
-		GIT_TAG        b796f7d44681514f58a683a3a71ff17c94edb0c1
+		GIT_TAG        v1.17.0
 )
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)  # For Windows: Prevent overriding the parent project's compiler/linker settings
 
