e7a29367fee289ba23cbfc7646fb30093f90ea21
Oliver Benz <oliver.benz@outlook.com>
2025-12-26 19:56:11 +0100

    CMake: Fix ide directory.

--- lib/FetchLibraries.cmake
+++ lib/FetchLibraries.cmake
@@ -11,7 +11,7 @@ FetchContent_Declare(
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)  # For Windows: Prevent overriding the parent project's compiler/linker settings
 
 FetchContent_MakeAvailable(googletest)
-set_target_properties(gtest      PROPERTIES FOLDER "${ideFolderExternal}")
-set_target_properties(gtest_main PROPERTIES FOLDER "${ideFolderExternal}")
-set_target_properties(gmock      PROPERTIES FOLDER "${ideFolderExternal}")
-set_target_properties(gmock_main PROPERTIES FOLDER "${ideFolderExternal}")
+set_target_properties(gtest      PROPERTIES FOLDER "${IDE_FOLDER_EXTERNAL}")
+set_target_properties(gtest_main PROPERTIES FOLDER "${IDE_FOLDER_EXTERNAL}")
+set_target_properties(gmock      PROPERTIES FOLDER "${IDE_FOLDER_EXTERNAL}")
+set_target_properties(gmock_main PROPERTIES FOLDER "${IDE_FOLDER_EXTERNAL}")
