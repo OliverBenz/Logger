2a7931aa0aaa2d61c8aba57629bd840695397ab5
Oliver Benz <oliver.benz@outlook.com>
2025-12-26 15:46:15 +0100

    CMake: Fix installation path.

--- lib/FetchLibraries.cmake
+++ lib/FetchLibraries.cmake
@@ -15,6 +15,3 @@ set_target_properties(gtest      PROPERTIES FOLDER "${ideFolderExternal}")
 set_target_properties(gtest_main PROPERTIES FOLDER "${ideFolderExternal}")
 set_target_properties(gmock      PROPERTIES FOLDER "${ideFolderExternal}")
 set_target_properties(gmock_main PROPERTIES FOLDER "${ideFolderExternal}")
-
-set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
-set(INSTALL_GMOCK OFF CACHE BOOL "" FORCE)
